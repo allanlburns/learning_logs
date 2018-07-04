d3eb700eb355e21c8c282f3afa12b2649a1d47ce
Allan Burns <allanlburns@yahoo.com>
2018-07-04 16:36:32 -0400

    Add new topic, new entry. Fix problem with showing topic page.

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -10,7 +10,7 @@ class TopicForm(forms.ModelForm):
 
 class EntryForm(forms.ModelForm):
 	class Meta:
-		model: Entry
+		model = Entry
 		fields = ['text']
 		labels = {'text': ''}
 		widgets = {'text': forms.Textarea(attrs={'cols': 80})}
